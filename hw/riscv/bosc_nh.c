43ed975bfe962449b3a8ce33fe19b05f7f541394
Yu Hu <huyu@bosc.ac.cn>
2024-06-19 11:19:28 +0800

    Modify the serial port configuration to match the nanhu-v3.dts
    Signed-off-by: Yu Hu <huyu@bosc.ac.cn>

--- hw/riscv/bosc_nh.c
+++ hw/riscv/bosc_nh.c
@@ -1515,7 +1515,7 @@ static void virt_machine_init(MachineState *machine)
     create_platform_bus(s, mmio_irqchip);
 
     serial_mm_init(system_memory, memmap[VIRT_UART0].base,
-        0, qdev_get_gpio_in(mmio_irqchip, UART0_IRQ), 399193,
+        2, qdev_get_gpio_in(mmio_irqchip, UART0_IRQ), 399193,
         serial_hd(0), DEVICE_LITTLE_ENDIAN);
 
     for (i = 0; i < ARRAY_SIZE(s->flash); i++) {
