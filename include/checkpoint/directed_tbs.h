6036429037aa75e38c0cb6c128cb167f080ca7c5
xiaokamikami <fengkehan@bosc.ac.cn>
2024-06-25 19:00:50 +0800

    add output the path of ckpt to fifo

--- include/checkpoint/directed_tbs.h
+++ include/checkpoint/directed_tbs.h
@@ -1,3 +1,6 @@
+#ifndef DIRECTED_TBS_H
+#define DIRECTED_TBS_H
+
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -7,6 +10,7 @@ typedef struct Qemu2Detail {
     bool cpt_ready;
     uint32_t cpt_id;
     uint64_t total_inst_count;
+    char checkpoint_path[256];
 } Qemu2Detail;
 
 typedef struct Detail2Qemu {
@@ -14,8 +18,9 @@ typedef struct Detail2Qemu {
     bool has_wfi[MAX_HARTS];
 } Detail2Qemu;
 
-
 typedef struct SyncControlInfo {
     Detail2Qemu u_arch_info;
     int info_vaild_periods;
 } SyncControlInfo;
+
+#endif
