501e7cf6d9b28ac247270a25d9a8b4a53fcd6c2b
Ran Wang <wangran@bosc.ac.cn>
2024-07-31 16:24:12 +0800

    riscv: nanhu: Fix illegal instrunciton exception
    Signed-off-by: Ran Wang <wangran@bosc.ac.cn>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -525,8 +525,8 @@ static void rv64_bosc_nanhu_cpu_init(Object *obj)
     CPURISCVState *env = &RISCV_CPU(obj)->env;
     RISCVCPU *cpu = RISCV_CPU(obj);
 
-    riscv_cpu_set_misa_ext(env, RVI | RVM | RVA | RVF | RVD | RVC | RVS | RVU);
-    env->priv_ver = PRIV_VERSION_1_10_0;
+    riscv_cpu_set_misa_ext(env, RVI | RVM | RVA | RVF | RVD | RVC | RVS | RVU | RVB);
+    env->priv_ver = PRIV_VERSION_1_12_0;
 #ifndef CONFIG_USER_ONLY
     set_satp_mode_max_supported(RISCV_CPU(obj), VM_1_10_SV39);
 #endif
