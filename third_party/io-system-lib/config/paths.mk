43d118105e3e99a8a19023cfff57f7136fbb5eca
Yaxing Guo <guoyaxing@bosc.ac.cn>
2026-08-20 10:16:47 +0800

    io-system: use QEMU's vendored my-virtio library
    Build the vendored io-system-lib against qemu/my-virtio-lib when it
    is included from QEMU's third_party directory. This keeps QEMU and
    its io-system dependency self-contained.
    
    Standalone io-system-lib workspaces retain their workspace-level
    my-virtio-lib path.
    
    Signed-off-by: Yaxing Guo <guoyaxing@bosc.ac.cn>

--- third_party/io-system-lib/config/paths.mk
+++ third_party/io-system-lib/config/paths.mk
@@ -6,13 +6,11 @@
 IO_SYSTEM_PARENT_DIR := $(notdir $(patsubst %/,%,$(dir $(IO_SYSTEM_ROOT))))
 ifeq ($(IO_SYSTEM_PARENT_DIR),third_party)
 WORKSPACE_ROOT ?= $(abspath $(IO_SYSTEM_ROOT)/../../..)
+MY_VIRTIO_LIB_DIR ?= $(abspath $(IO_SYSTEM_ROOT)/../../my-virtio-lib)
 else
 WORKSPACE_ROOT ?= $(abspath $(IO_SYSTEM_ROOT)/..)
+MY_VIRTIO_LIB_DIR ?= $(abspath $(WORKSPACE_ROOT)/my-virtio-lib)
 endif
-# XSV vendors libvirtio beside contrib/qemu.  Keep the standalone workspace
-# layout working until it is migrated to the same directory name.
-MY_VIRTIO_LIB_DIR ?= $(abspath $(if $(wildcard $(WORKSPACE_ROOT)/libvirtio), \
-	$(WORKSPACE_ROOT)/libvirtio,$(WORKSPACE_ROOT)/my-virtio-lib))
 BOSC_IOMMU_DIR ?= $(abspath $(WORKSPACE_ROOT)/bosc-iommu-v2)
 UNITYCHIP_ROOT ?= $(abspath $(BOSC_IOMMU_DIR)/third_party/unitychip)
 
